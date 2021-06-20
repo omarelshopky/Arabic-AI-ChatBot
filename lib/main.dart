211496c7f3df9a3a46d58a61ae3ce01ae2c61936
Omar Hesham <oommaarr.220022@gmail.com>
2021-06-20 18:08:48 +0200

    Handle RTL Arabic according to device settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // @dart=2.9
 
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'chatbot_screen.dart';
 
 // ignore: slash_for_doc_comments
@@ -14,6 +15,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      /** Handle arabic RTL and English LTL */
+      localizationsDelegates: [
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale("en", "US"),
+        Locale("ar", "AE"), 
+      ],
+      /**** */
       debugShowCheckedModeBanner: false,
       title: "ChatBot",
       home: ChatBotScreen(),
