888b85d8ffd4ca898411737c024af960ad53692a
Omar Hesham <oommaarr.220022@gmail.com>
2021-06-21 02:27:23 +0200

    Add more Q&A to the App

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'home_screen.dart';
 import 'chatbot_screen.dart';
 
 // ignore: slash_for_doc_comments
