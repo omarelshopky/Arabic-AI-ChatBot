06be4a63e52096323c1e0f26fbcfd4aa42c53940
Omar Hesham <oommaarr.220022@gmail.com>
2021-07-01 01:48:55 +0200

    Add dropdown Text code to info_screen

--- lib/info_screen.dart
+++ lib/info_screen.dart
@@ -4,6 +4,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts_arabic/fonts.dart';
+import 'package:groovin_widgets/groovin_widgets.dart';
 
 enum category_t{
   CPU, MOTHER_BOARD, RAM, POWER_SUPPLY, COMPUTER_CASE, IO_UNITS
@@ -34,6 +35,63 @@ class _InfoScreenState extends State<InfoScreen> {
           )
         ),
       ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 10, left: 7, right: 7),
+            child: GroovinExpansionTile(
+              title: Text('Name'),
+              boxDecoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(7),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey[300],
+                    offset: const Offset(2.0, 2.0), //Offset
+                    blurRadius: 2.0,
+                    spreadRadius: 1.0,
+                  ), //BoxShadow
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: const Offset(0.0, 0.0),
+                    blurRadius: 0.0,
+                    spreadRadius: 0.0,
+                  ), 
+                ],
+              ),
+              children: [
+                Text('omar hesham ali')
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10, left: 7, right: 7),
+            child: GroovinExpansionTile(
+              title: Text('sososo'),
+              boxDecoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(7),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey[300],
+                    offset: const Offset(2.0, 2.0), //Offset
+                    blurRadius: 2.0,
+                    spreadRadius: 1.0,
+                  ), //BoxShadow
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: const Offset(0.0, 0.0),
+                    blurRadius: 0.0,
+                    spreadRadius: 0.0,
+                  ), 
+                ],
+              ),
+              children: [
+                Text('tot')
+              ],
+            ),
+          )
+        ],
+      )
+        
     );
   }
 }
