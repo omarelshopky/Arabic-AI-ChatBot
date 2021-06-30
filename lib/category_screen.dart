88813dc5f62413d7226840632cfc814b2c151257
Omar Hesham <oommaarr.220022@gmail.com>
2021-06-30 22:46:53 +0200

    Add appbar to category_screen

--- lib/category_screen.dart
+++ lib/category_screen.dart
@@ -1,8 +1,10 @@
 // @dart=2.9
 
 import 'package:flutter/material.dart';
-import 'chatbot_screen.dart';
+import 'package:google_fonts_arabic/fonts.dart';
+import 'package:relative_scale/relative_scale.dart';
 
+import 'chatbot_screen.dart';
 
 
 class CategoryScreen extends StatefulWidget {
@@ -15,7 +17,19 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFFFCB05), // AppBar Color
+        centerTitle: true,
+        title: Text(
+          "الصفحة الرئيسية",
+          style: TextStyle(
+            fontSize: 28, 
+            fontFamily: ArabicFonts.El_Messiri,
+            package: 'google_fonts_arabic',
+          )
+        ),
+      ),
       
     );
   }
