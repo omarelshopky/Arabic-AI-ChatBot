69437abf1c3bc0192807ed4e247aef755f854d29
Omar Hesham <oommaarr.220022@gmail.com>
2021-06-30 03:28:03 +0200

    Add logo and linear gradient to home home_screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:relative_scale/relative_scale.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({ Key? key }) : super(key: key);
@@ -11,7 +12,30 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+        decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+              colors: [
+                const Color(0xFF3cae5a),
+                const Color(0xFF079c92),
+              ],
+              begin: const FractionalOffset(0.0, 0.3),
+                end: const FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp),
+        ),
+        child: Align(
+          alignment: Alignment.center,
+          
+          child: RelativeBuilder( 
+            builder: (context, height, width, sy, sx) {
+              return Image.asset(
+                "images/logo.png",
+                width: sx(320),
+                height: sy(320),
+              );
+            }
+          ),
+        )
 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
       /**** */
       debugShowCheckedModeBanner: false,
       title: "ChatBot",
-      home: ChatBotScreen(),
+      home: HomeScreen(),
     );
   }
 } 
