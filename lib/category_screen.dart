e3d3f7f409260cbe7f9b102f4004f601f2dedcef
Omar Hesham <oommaarr.220022@gmail.com>
2021-06-30 23:04:22 +0200

    Add Cpu and motherBoard button to category_screen

--- lib/category_screen.dart
+++ lib/category_screen.dart
@@ -30,7 +30,78 @@ class _CategoryScreenState extends State<CategoryScreen> {
           )
         ),
       ),
-      
+      body: RelativeBuilder( 
+        builder: (context, height, width, sy, sx) {
+          return Column(
+            children: [
+              Row( // First Row
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: sy(25), left: sx(40), right: sx(15)),
+                    child: Column(
+                      children: [
+                        OutlinedButton(
+                          onPressed: (){
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => CategoryScreen())
+                            );
+                          },
+                          child: Container(
+                            child: Image.asset(
+                              "images/cpu.png",
+                              width: sx(170),
+                              height: sy(100),
+                            )
+                          ),
+                          style: OutlinedButton.styleFrom(
+                            primary: Colors.white,
+                            side: BorderSide(color: Colors.black, width: 0.7),
+                            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                          ),
+                        ),
+                        Text(
+                          'CPU'
+                        )
+                      ],
+                    )
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: sy(25), left: sx(15)),
+                    child: Column(
+                      children: [
+                        OutlinedButton(
+                          onPressed: (){
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => CategoryScreen())
+                            );
+                          },
+                          child: Container(
+                            child: Image.asset(
+                              "images/motherBoard.png",
+                              width: sx(170),
+                              height: sy(100),
+                            )
+                          ),
+                          style: OutlinedButton.styleFrom(
+                            primary: Colors.white,
+                            side: BorderSide(color: Colors.black, width: 0.7),
+                            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                          ),
+                        ),
+                        Text(
+                          'Mother Board'
+                        )
+                      ],
+                    )
+                  )
+                ],
+              )
+            ],
+          );
+        },
+      ), 
     );
   }
 }
